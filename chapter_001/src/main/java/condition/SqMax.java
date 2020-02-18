4f126b329440d49c22ef344543360cd09f32c56d
Kirbri <kanna1997@mail.ru>
2020-02-18 17:27:00 +0300

    4.5. Отладка программы в IDEA[#236420]

---
+++ chapter_001/src/main/java/condition/SqMax.java
@@ -0,0 +1,32 @@
+package condition;
+
+public class SqMax {
+    public static int max(int first, int second, int third, int forth) {
+        int result = forth;
+        if (first > second) {
+            if (first > third) {
+                if (first > forth) {
+                    result = first;
+                }
+                else result = forth;
+            }
+            else if (third > forth) {
+                result = third;
+            }
+
+        } else {
+            if (second > third) {
+                if (second>forth) {
+                    result = second;
+                }
+            }
+            else {
+                if (third>forth){
+                    result = third;
+                }
+                else result = forth;
+            }
+        }
+        return result;
+    }
+}
