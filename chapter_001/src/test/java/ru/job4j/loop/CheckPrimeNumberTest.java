717bcdbcbeba16bd21228dc23677a96cbdc3b7ce
Kirbri <kanna1997@mail.ru>
2020-02-24 14:51:48 +0300

    5.5. Простое число.[#236501]

--- chapter_001/src/test/java/ru/job4j/loop/CheckPrimeNumberTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/CheckPrimeNumberTest.java
@@ -29,4 +29,10 @@ public class CheckPrimeNumberTest {
         boolean rsl = CheckPrimeNumber.check(9);
         assertThat(rsl, is(false));
     }
+
+    @Test
+    public void when2() {
+        boolean rsl = CheckPrimeNumber.check(2);
+        assertThat(rsl, is(true));
+    }
 }
