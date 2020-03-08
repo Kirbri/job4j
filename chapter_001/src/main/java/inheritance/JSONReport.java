e4be05523278acfa05ec59fa3738330403eb9434
Kirbri <kanna1997@mail.ru>
2020-03-08 17:19:30 +0300

    4. Переопределение[#236610]

--- chapter_001/src/main/java/inheritance/JSONReport.java
+++ chapter_001/src/main/java/inheritance/JSONReport.java
@@ -2,9 +2,13 @@ package inheritance;
 
 public class JSONReport extends TextReport {
 
+    private static final String LS = System.lineSeparator();
+
     @Override
     public String generate(String name, String body) {
-        return "name: " + name + System.lineSeparator()+
-                "body: " + body;
+        return "{"+ LS + LS +
+                "  name: " + name + LS +
+                "  body: " + body + LS + LS +
+                "}";
     }
 }
