80d297b5d4ae8dabf697ec06f933a27d867973e1
Kirbri <kanna1997@mail.ru>
2020-03-21 19:41:16 +0300

    2. Scanner и чтение числа из консоли.[#236566]

--- chapter_001/src/main/java/ru/job4j/ru/job4j/io/Matches.java
+++ chapter_001/src/main/java/ru/job4j/ru/job4j/io/Matches.java
@@ -6,24 +6,39 @@ public class Matches {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int matches = 11;
+        String player = "Победил первый игрок!";
 
         System.out.println("Играет два человека, вы можете взять от 1 до 3 спичек!");
         System.out.println();
         do {
             System.out.println("Спичек на столе - " + matches);
             System.out.print("Сколько первый игрок заберёт спичек? ");
-            matches = matches - Integer.valueOf(input.nextLine());
+
+            int value = Integer.valueOf(input.nextLine());
+            if (value>=1 & value<=3) {
+                matches = matches - value;
+            } else {
+                System.out.println("Вы ввели некорректное число!");
+            }
+            if (matches <= 0) {
+                break;
+            }
             System.out.println("Спичек на столе - " + matches);
-            if (matches == 0){
-                System.out.println("Первый игрок выиграл, УРА!");
-                break;
-            }
+
             System.out.print("Сколько второй игрок заберёт спичек? ");
-            matches = matches - Integer.valueOf(input.nextLine());
-            if (matches == 0){
-                System.out.println("Второй игрок выиграл, УРА!");
+
+            value = Integer.valueOf(input.nextLine());
+            if (value>=1 & value<=3) {
+                matches = matches - value;
+            } else {
+                System.out.println("Вы ввели некорректное число!");
+            }
+            if (matches <= 0) {
+                player = "Победил второй игрок!";
                 break;
             }
+
         } while (matches > 0);
+        System.out.println(player);
     }
 }
