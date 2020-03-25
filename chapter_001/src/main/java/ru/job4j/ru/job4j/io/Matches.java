ca3c644e6f61fe0d84bb5d722333f70bce956938
Kirbri <kanna1997@mail.ru>
2020-03-25 23:32:42 +0300

    2. Scanner и чтение числа из консоли.[#236566]

--- chapter_001/src/main/java/ru/job4j/ru/job4j/io/Matches.java
+++ chapter_001/src/main/java/ru/job4j/ru/job4j/io/Matches.java
@@ -3,42 +3,42 @@ package ru.job4j.ru.job4j.io;
 import java.util.Scanner;
 
 public class Matches {
+    public static int infoAboutGamer(int gamer) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Сколько игрок " + gamer + "  заберёт спичек? ");
+        int value = Integer.valueOf(input.nextLine());
+        int result = 0;
+        if (value>=1 & value<=3) {
+            result = value;
+            System.out.println("Вы взяли корректное число спичек!");
+        } else {
+            System.out.println("Вы не можете взять столько спичек! Посмотрите условия игры!");
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int matches = 11;
-        String player = "Победил первый игрок!";
 
-        System.out.println("Играет два человека, вы можете взять от 1 до 3 спичек!");
+        System.out.println("Играет два человека, вы можете взять от 1 до 3 спичек! Победит тот, кто возьмёт последние спички!");
         System.out.println();
-        do {
-            System.out.println("Спичек на столе - " + matches);
-            System.out.print("Сколько первый игрок заберёт спичек? ");
-
-            int value = Integer.valueOf(input.nextLine());
-            if (value>=1 & value<=3) {
-                matches = matches - value;
-            } else {
-                System.out.println("Вы ввели некорректное число!");
-            }
+        while (matches > 0) {
+            matches -= infoAboutGamer(1);
             if (matches <= 0) {
-                break;
-            }
-            System.out.println("Спичек на столе - " + matches);
-
-            System.out.print("Сколько второй игрок заберёт спичек? ");
-
-            value = Integer.valueOf(input.nextLine());
-            if (value>=1 & value<=3) {
-                matches = matches - value;
-            } else {
-                System.out.println("Вы ввели некорректное число!");
-            }
-            if (matches <= 0) {
-                player = "Победил второй игрок!";
+                System.out.println("Первый игрок победил!");
                 break;
             }
 
-        } while (matches > 0);
-        System.out.println(player);
+            System.out.println("На столе " + matches + " спичек.");
+
+            matches -= infoAboutGamer(2);
+            if (matches <= 0) {
+                System.out.println("Второй игрок победил!");
+                break;
+            }
+
+            System.out.println("На столе " + matches + " спичек.");
+        }
     }
 }
