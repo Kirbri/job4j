3b65c91673069c207ef0688249a4c733fa0fe721
Kirbri <kanna1997@mail.ru>
2020-04-06 20:39:07 +0300

    9. Написать тесты на StartUI.[#236577]

--- chapter_001/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_001/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -55,4 +55,14 @@ public class StartUITest {
         Item deleted = tracker.findById(item.getId());
         assertNull(deleted);
     }
+
+    @Test
+    public void whenExit() {
+        StubInput input = new StubInput(
+                new String[] {"0"}
+        );
+        StubAction action = new StubAction();
+        new StartUI().init(input, new Tracker(), new UserAction[] { action });
+        assertThat(action.isCall(), is(true));
+    }
 }
