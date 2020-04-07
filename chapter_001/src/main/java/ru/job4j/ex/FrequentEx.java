b6de27703518308198f7f634c6ff50ad39d9f18c
Kirbri <kanna1997@mail.ru>
2020-04-07 22:58:49 +0300

    0.1. java.lang.NullPointerException[#236583]

--- chapter_001/src/main/java/ru/job4j/ex/FrequentEx.java
+++ chapter_001/src/main/java/ru/job4j/ex/FrequentEx.java
@@ -3,7 +3,7 @@ package ru.job4j.ex;
 public class FrequentEx {
     public static void main(String[] args) {
         String[] shops = {"Ebay", "Amazon", "Ozon"};
-        for(int index = 0; index < shops.length; index++){
+        for (int index = 0; index < shops.length; index++) {
             System.out.println(shops[index]);
         }
     }
