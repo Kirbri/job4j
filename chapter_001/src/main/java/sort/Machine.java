d7f376f8e12aa1ee73b1c3819958c6ac1bc8521f
Kirbri <kanna1997@mail.ru>
2020-04-26 23:09:35 +0300

    1. Контрольные вопросы.[#236625]

--- chapter_001/src/main/java/sort/Machine.java
+++ chapter_001/src/main/java/sort/Machine.java
@@ -10,37 +10,12 @@ public class Machine {
         int[] rsl = new int[100];
         int size = 0;
         int surrender = money - price;
-
-
-        for (int i = 0; i < COINS.length; i++) { //третий вариант
-            while (surrender >= COINS[i]) {
-                surrender -= COINS[i];
-                rsl[size++] = COINS[i];
+        for (int index : COINS) {
+            while (surrender >= index) {
+                surrender -= index;
+                rsl[size++] = index;
             }
         }
-
-//        for (int index : COINS) {// Второй вариант реализации
-//            while (surrender >= index) {
-//                surrender -= index;
-//                rsl[size++] = index;
-//            }
-//        }
-
-//        int surrender = money - price; //Первый вариант, мой, который никому не понравился :D
-//        while (surrender > 0) {
-//            for (int i = 0; i < COINS.length;) {
-//                if (surrender - COINS[i] >= 0) {
-//                    surrender -= COINS[i];
-//                    rsl[size++] = COINS[i];
-//                } else {
-//                    i++;
-//                }
-//            }
-//        }
         return Arrays.copyOf(rsl, size);
-    }
-
-    public static void main(String[] args) {
-        System.out.println(Arrays.toString(new Machine().change(40, 10)));
     }
 }
