ebc3cbe785fe27ce0e763d554a6a28025a0b3487
Kirbri <kanna1997@mail.ru>
2020-05-22 16:39:19 +0300

    3. Банковские переводы.[10038#236541]

--- chapter_001/src/main/java/ru/job4j/bank/BankService.java
+++ chapter_001/src/main/java/ru/job4j/bank/BankService.java
@@ -13,27 +13,29 @@ public class BankService {
     }
 
     public void addAccount(String passport, Account account) {
-        if (findByPassport(passport) != null) {
-             if (!users.get(findByPassport(passport)).contains(account)) {
-                 users.get(findByPassport(passport)).add(account);
-             }
+        User user = findByPassport(passport);
+        if (user != null) {
+            if (!users.get(user).contains(account)) {
+                users.get(user).add(account);
+            }
         }
     }
 
     public User findByPassport(String passport) {
-        for (User user : users.keySet()) {
-            if (user.getPassport().equals(passport)) {
-                return user;
+        for (User value : users.keySet()) {
+            if (value.getPassport().equals(passport)) {
+                return value;
             }
         }
         return null;
     }
 
     public Account findByRequisite(String passport, String requisite) {
-        if (findByPassport(passport) != null) {
-            for (Account value : users.get(findByPassport(passport))) {
-                if (value.getRequisite().equals(requisite)) {
-                    return value;
+        User user = findByPassport(passport);
+        if (user != null) {
+            for (Account account : users.get(user)) {
+                if (account.getRequisite().equals(requisite)) {
+                    return account;
                 }
             }
         }
@@ -41,21 +43,13 @@ public class BankService {
     }
 
     public boolean transferMoney(String srcPassport, String srcRequisite, String destPassport, String destRequisite, double amount) {
-        Account accSrc = findByRequisite(srcPassport, srcRequisite);
-        Account accDest = findByRequisite(destPassport, destRequisite);
-        if (accSrc != null && accDest != null && accSrc.getBalance() >= amount) {
-            accSrc.setBalance(accSrc.getBalance() - amount);
-            accDest.setBalance(accDest.getBalance() + amount);
+        Account accountSrc = findByRequisite(srcPassport, srcRequisite);
+        Account accountDest = findByRequisite(destPassport, destRequisite);
+        if (accountSrc != null && accountDest != null && accountSrc.getBalance() >= amount) {
+            accountSrc.setBalance(accountSrc.getBalance() - amount);
+            accountDest.setBalance(accountDest.getBalance() + amount);
             return true;
         }
         return false;
-    }
-
-    public static void main(String[] args) {
-        User user = new User("3434", "Petr Arsentev");
-        BankService bank = new BankService();
-        bank.addUser(user);
-        bank.addAccount(user.getPassport(), new Account("5546", 150D));
-        System.out.println(bank.findByRequisite("3434", "5546"));
     }
 }
